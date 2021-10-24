53ddedc96d7365e796de8edb9713b5a897d8d97d
birdbrained <mwk9@njit.edu>
2021-10-23 22:46:21 -0400

    Add Crap class, initial code to support random crap generation

---
+++ Assets/Scripts/Crap.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CrapCategory
+{
+    CC_NULL = 0,
+    CC_Sports = 1,
+    CC_Food = 2,
+    CC_KnickKnack = 3,
+    CC_Electronic = 4,
+    CC_Clothes = 5,
+    CC_Kitchenware = 6,
+//    CC_PetRelated = 7,
+    CC_Games = 8,
+    CC_Furniture = 9
+};
+
+public class Crap
+{
+    //[SerializeField]
+    private int crapID = 0;
+    public int CrapID
+    {
+        get
+        {
+            return crapID;
+        }
+    }
+    //[SerializeField]
+    private string crapName = "undefined";
+    public string CrapName
+    {
+        get
+        {
+            return crapName;
+        }
+    }
+    //[SerializeField]
+    private string crapDescription = "undefined";
+    public string CrapDescription
+    {
+        get
+        {
+            return crapDescription;
+        }
+    }
+    //[SerializeField]
+    private CrapCategory crapType = CrapCategory.CC_NULL;
+    public CrapCategory CrapType
+    {
+        get
+        {
+            return crapType;
+        }
+    }
+    //[SerializeField]
+    private float crapMsrp = 0.00f;
+    public float CrapMsrp
+    {
+        get
+        {
+            return crapMsrp;
+        }
+    }
+    //[SerializeField]
+    private string imageFilepath = "Sprites/none";
+    //[SerializeField]
+    private Sprite imageSprite;
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    public void SetupCrap(int id, string name, string desc, CrapCategory type, float msrp, string imgFilepath)
+    {
+        crapID = id;
+        crapName = name;
+        crapDescription = desc;
+        crapType = type;
+        crapMsrp = msrp;
+        imageFilepath = imgFilepath;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    private List<Crap> masterCrapList;// = new List<Crap>();
+    public List<Crap> MasterCrapList
+    {
+        get
+        {
+            return masterCrapList;
+        }
+    }
+
+    [SerializeField]
+    private Text object1;
+    [SerializeField]
+    private Text object2;
+    [SerializeField]
+    private Text object3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        masterCrapList = new List<Crap>();
+        SetupCrapList();
+        PrintCrapList();
+
+        GenerateRoundCrap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetupCrapList()
+    {
+        string path = "Assets/Definitions/all_crap.txt";
+        //StreamReader reader = new StreamReader(path);
+        string[] lines = System.IO.File.ReadAllLines(path);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            //Debug.Log(i.ToString() + ": " + lines[i]);
+            string[] crapInfo = lines[i].Split('\t');
+            for (int j = 0; j < crapInfo.Length; j++)
+            {
+                //Debug.Log(i.ToString() + "-" + j.ToString() + ": " + crapInfo[j]);
+                Crap c = new Crap();
+                c.SetupCrap(int.Parse(crapInfo[0]),
+                    crapInfo[1],
+                    crapInfo[2],
+                    (CrapCategory)int.Parse(crapInfo[3]),
+                    float.Parse(crapInfo[4]),
+                    crapInfo[5]
+                );
+                masterCrapList.Add(c);
+            }
+        }
+    }
+
+    public void PrintCrapList()
+    {
+        foreach (Crap c in masterCrapList)
+        {
+            Debug.Log(c.CrapName);
+        }
+    }
+
+    public void GenerateRoundCrap()
+    {
+        if (masterCrapList.Count < 3)
+            return;
+
+        int i1 = Random.Range(0, masterCrapList.Count);
+        int i2 = Random.Range(0, masterCrapList.Count);
+        while (i2 == i1)
+            i2 = Random.Range(0, masterCrapList.Count);
+        int i3 = Random.Range(0, masterCrapList.Count);
+        while (i3 == i1 && i3 == i2)
+            i3 = Random.Range(0, masterCrapList.Count);
+
+        if (object1 != null)
+            object1.text = masterCrapList[i1].CrapName;
+        if (object2 != null)
+            object2.text = masterCrapList[i2].CrapName;
+        if (object3 != null)
+            object3.text = masterCrapList[i3].CrapName;
+    }
+}
